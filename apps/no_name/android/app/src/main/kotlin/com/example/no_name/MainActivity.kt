a9e1f6c581e9348f375eba7c238c78e0074b184c
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-06-16 14:01:04 +0530

    chore: remove no_name app

--- apps/no_name/android/app/src/main/kotlin/com/example/no_name/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.no_name
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- apps/no_name/lib/src/core/api/api_client.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:network/network.dart';
-
-final apiClientProvider = Provider<ApiClient>((ref) => ApiClient(apiExecutor: DioApiExecutor()));
