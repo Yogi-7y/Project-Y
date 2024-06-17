0f27c269bc1c26a34e3b9ad61ca5e76931bb841d
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-06-17 18:12:55 +0530

    chore(notion_inbox): bump smart_textfield

--- apps/notion_inbox/lib/main.dart
+++ apps/notion_inbox/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:network/network.dart';
 
 import 'src/app.dart';
-import 'src/services/api_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
